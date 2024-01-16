b199a0a058d860b13bd1f84db2152e25e12d7134
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-01-16 12:35:24 +0100

    Deploy

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,36 +1,36 @@
-    #!/usr/bin/env bash
+#!/usr/bin/env bash
 
-    # Instalar dependencias de Composer
-    echo "Running composer..."
-    composer global require hirak/prestissimo
-    composer install --no-dev --working-dir=/var/www/html
+# Instalar dependencias de Composer
+echo "Running composer..."
+composer global require hirak/prestissimo
+composer install --no-dev --working-dir=/var/www/html
 
-    # Generar clave de aplicación
-    echo "Generating application key..."
-    php artisan key:generate --show
+# Generar clave de aplicación
+echo "Generating application key..."
+php artisan key:generate --show
 
-    # Cachear configuración
-    echo "Caching config..."
-    php artisan config:cache
+# Cachear configuración
+echo "Caching config..."
+php artisan config:cache
 
-    # Cachear rutas
-    echo "Caching routes..."
-    php artisan route:cache
+# Cachear rutas
+echo "Caching routes..."
+php artisan route:cache
 
-    # Ejecutar migraciones
-    echo "Running migrations..."
-    php artisan migrate --force
+# Ejecutar migraciones
+echo "Running migrations..."
+php artisan migrate --force
 
-    #Ponemos datos de prueba
-    echo "Seeding the database..."
-    php artisan db:seed
+#Ponemos datos de prueba
+echo "Seeding the database..."
+php artisan db:seed
 
-    # Aquí comienzan los pasos específicos de Vite
-    echo "Installing Node.js dependencies..."
-    npm install
+# Instalar dependencias de Node.js para Vite
+echo "Installing Node.js dependencies..."
+npm install
 
-    echo "Building front-end assets with Vite..."
-    npm run production
-    npm run dev
+# Construir los assets de front-end utilizando Vite
+echo "Building front-end assets with Vite..."
+npm run build
 
-    # Continúa con cualquier otro paso necesario para tu despliegue
+# Continúa con cualquier otro paso necesario para tu despliegue
