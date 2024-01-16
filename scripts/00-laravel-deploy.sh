63df60298c6802b697ec120c7321085f6198b3b9
JavierJamaicaCast <javiernicolasjamaica@gmail.com>
2024-01-16 13:22:04 +0100

    Deploy with new script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -14,14 +14,9 @@ echo "Actualizando el código fuente..."
 cd $APP_DIR
 git pull origin main  # Reemplaza 'main' con la rama que estés utilizando
 
-# Instala dependencias de Composer
-echo "Instalando dependencias de Composer..."
-$COMPOSER_BIN global require hirak/prestissimo
-$COMPOSER_BIN install --no-dev
-
-# Actualiza el autoloader de Composer
-echo "Actualizando el autoloader de Composer..."
-$COMPOSER_BIN dump-autoload --working-dir=$APP_DIR
+# Instala o actualiza las dependencias de Composer
+echo "Instalando o actualizando dependencias de Composer..."
+$COMPOSER_BIN install --no-dev --optimize-autoloader
 
 # Genera una clave de aplicación si no está configurada
 if [ ! -f "$APP_DIR/$ENV_FILE" ]; then
@@ -38,16 +33,12 @@ $PHP_BIN $ARTISAN_BIN route:cache
 echo "Ejecutando migraciones de base de datos..."
 $PHP_BIN $ARTISAN_BIN migrate --force
 
-# Poner datos de prueba
-echo "Seeding the database..."
-$PHP_BIN $ARTISAN_BIN db:seed
-
 # Instala dependencias de Node.js (opcional, si se utilizan)
 if [ -f "$APP_DIR/package.json" ]; then
     echo "Instalando dependencias de Node.js..."
     cd $APP_DIR
     $NPM_BIN install
-    $NPM_BIN run build  # Otra opción como 'dev' dependiendo de tu entorno
+    $NPM_BIN run production  # Otra opción como 'dev' dependiendo de tu entorno
 fi
 
 # Reinicia Nginx
@@ -57,8 +48,5 @@ $NGINX_RESTART_CMD
 # Limpia la caché de la aplicación Laravel
 echo "Limpiando la caché de Laravel..."
 $PHP_BIN $ARTISAN_BIN cache:clear
-$PHP_BIN $ARTISAN_BIN config:clear
-
-# Otras tareas de implementación, como la configuración de permisos, pueden ir aquí
 
 echo "¡Despliegue completado!"
